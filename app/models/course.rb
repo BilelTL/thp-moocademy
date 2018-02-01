2aa60b8a88a800ac16bb754bc20498f1badab7de
Triki Bilel <triki.bilel0033@gmal.com>
2018-02-01 17:46:37 +0100

    v2

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,5 +1,5 @@
 class Course < ApplicationRecord
-	has_many :lessons 
+	has_many :lessons, dependent: :destroy 
 
 	validates :title, presence: true, length: {minimum: 1}
 	validates :description, presence: true
